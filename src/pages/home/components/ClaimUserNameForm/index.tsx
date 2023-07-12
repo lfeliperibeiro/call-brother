5b8297b9cc7ac85e432a360106764f9735c9ab64
Felipe Ribeiro <piporibeiro@gmail.com>
2023-07-12 14:51:04 -0300

    feat: start new version

---
+++ src/pages/home/components/ClaimUserNameForm/index.tsx
@@ -0,0 +1,59 @@
+import { Form, FormAnnotation } from './styles'
+import { Button, Text, TextInput } from '@ribeiro-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/router'
+
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário pode ter apenas letras e hífens',
+    })
+    .transform((username) => username.toLowerCase()),
+})
+
+type ClaimUserNameFormData = z.infer<typeof claimUsernameFormSchema>
+
+export function ClaimUserNameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<ClaimUserNameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
+
+  const router = useRouter()
+  async function handleClaimUsernameForm(data: ClaimUserNameFormData) {
+    const { username } = data
+
+    await router.push(`/register?username=${username}`)
+  }
+  return (
+    <>
+      <Form as={'form'} onSubmit={handleSubmit(handleClaimUsernameForm)}>
+        <TextInput
+          size={'sm'}
+          prefix={'callbrother.com/'}
+          placeholder={'seu-usuario'}
+          {...register('username')}
+        />
+        <Button size={'sm'} type={'submit'} disabled={isSubmitting}>
+          Reservar
+          <ArrowRight />
+        </Button>
+      </Form>
+      <FormAnnotation>
+        <Text size={'sm'}>
+          {errors.username
+            ? errors.username?.message
+            : 'Dígite o nome do usuário desejado'}
+        </Text>
+      </FormAnnotation>
+    </>
+  )
+}
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,37 @@
+import { Container, Hero, Preview } from '@/src/pages/home/styles'
+import { Heading, Text } from '@ribeiro-ui/react'
+import Image from 'next/image'
+
+import previewImage from '../../assets/app_preview.png'
+import { ClaimUserNameForm } from '@/src/pages/home/components/ClaimUserNameForm'
+import { NextSeo } from 'next-seo'
+export default function Home() {
+  return (
+    <>
+      <NextSeo
+        title="Descomplique sua agenda | Ignite Call"
+        description="Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre."
+      />
+      <Container>
+        <Hero>
+          <Heading size={'4xl'}>Agendamento descomplicado</Heading>
+          <Text size={'xl'}>
+            Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre.
+          </Text>
+          <ClaimUserNameForm />
+        </Hero>
+        <Preview>
+          <Image
+            src={previewImage}
+            alt={'Calendário símbolizando a aplicação em funcionamento'}
+            quality={100}
+            priority
+            width={827}
+            height={442}
+          />
+        </Preview>
+      </Container>
+    </>
+  )
+}
